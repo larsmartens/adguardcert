fafa2a993cb683cd794342b4612a0d956983a5dc
Lars Martens <lars@mrtns.org>
2026-06-30 02:32:01 +0200

    fix: skip update metadata for prerelease tags

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-case version in
+VERSION=$(sed -ne "s/version=\(.*\)/\1/gp" module/module.prop)
+
+case "$VERSION" in
 *-*)
+    echo "Skipping update.json rewrite for prerelease $VERSION"
+    exit 0
     ;;
 *)
     git checkout -B master origin/master
